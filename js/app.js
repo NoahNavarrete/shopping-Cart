fbd6c6edc321ecb7bdabb250be0c1e340bfc9c19
Noah <noenavarrete71@outlook.com>
2023-06-28 18:36:01 -0300

    leer datos de los cursos

---
+++ js/app.js
@@ -0,0 +1,43 @@
+// variables
+const carrito = document.querySelector("#carrito");
+const contenedorCarrito = document.querySelector("#lista-carrito tbody");
+const vaciarCarritoBtn = document.querySelector("#vaciar-carrito");
+const listaCursos = document.querySelector("#lista-cursos");
+
+cargarEventListeners();
+function cargarEventListeners () {
+    // Cuando agregas un curso presionando "agregar al carrito"
+    listaCursos.addEventListener("click", agregarCurso);
+
+}
+
+
+// Funciones
+
+
+function agregarCurso(e) {
+    e.preventDefault()
+    if(e.target.classList.contains("agregar-carrito")) {
+        const cursoSeleccionado = e.target.parentElement.parentElement;
+        leerDatosCursos(cursoSeleccionado)
+    }
+    console.log();
+}
+
+// Lee el contenido del HTML al que le dimos click y extrae la informacion del curso
+function leerDatosCursos(curso) {
+    console.log(curso);
+
+    // Crear un objeto con el contenido del curos actual
+    const infoCurso = {
+        imagen: curso.querySelector("img").src,
+        titulo: curso.querySelector("h4").textContent,
+        precio: curso.querySelector(".precio span").textContent,
+        id: curso.querySelector("a").getAttribute("data-id"),
+        cantidad: 1
+    }
+
+    console.log(infoCurso);
+}
+
+
